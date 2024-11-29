efddb93b2fb36fc3bd46ec4fc88663e40d69b6ec
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-11-29 15:19:33 +0300

    поправил расчеты

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+# calculator.py
 from sqlalchemy.orm import sessionmaker
 from models import PaperCosts, TypicalOperations
 from database import engine
@@ -9,9 +10,7 @@ def calculate_documents_per_year(data):
     employee_count = data['employee_count']
     documents_per_employee = data['documents_per_employee']
     turnover_percentage = data['turnover_percentage']
-    return employee_count * (
-        documents_per_employee * (1 + turnover_percentage / 100)
-        )
+    return employee_count * (documents_per_employee * (1 + turnover_percentage / 100))
 
 
 def calculate_pages_per_year(data):
@@ -33,8 +32,7 @@ def calculate_total_paper_costs(pages_per_year):
 def calculate_total_logistics_costs(data, documents_per_year):
     courier_delivery_cost = data['courier_delivery_cost']
     hr_delivery_percentage = data.get('hr_delivery_percentage', 0)
-    return courier_delivery_cost * (
-        hr_delivery_percentage / 100 * documents_per_year)
+    return courier_delivery_cost * (hr_delivery_percentage / 100 * documents_per_year)
 
 
 def calculate_cost_per_minute(data):
@@ -43,8 +41,7 @@ def calculate_cost_per_minute(data):
     return average_salary / working_minutes_per_month
 
 
-def calculate_total_operations_costs(
-        data, documents_per_year, cost_per_minute):
+def calculate_total_operations_costs(data, documents_per_year, cost_per_minute):
     session = Session()
     typical_operations = session.query(TypicalOperations).first()
     session.close()
@@ -65,16 +62,24 @@ def calculate_total_operations_costs(
     return total_operations_costs
 
 
+def calculate_total_license_costs(data, license_costs):
+    hr_specialist_count = data['hr_specialist_count']
+    employee_count = data['employee_count']
+    return (
+        license_costs.hr_license_cost * hr_specialist_count +
+        license_costs.employee_license_cost * employee_count +
+        license_costs.main_license_cost
+    )
+
+
 def calculate_costs(data, paper_costs, license_costs, typical_operations):
     documents_per_year = calculate_documents_per_year(data)
     pages_per_year = calculate_pages_per_year(data)
     total_paper_costs = calculate_total_paper_costs(pages_per_year)
-    total_logistics_costs = calculate_total_logistics_costs(
-        data, documents_per_year)
+    total_logistics_costs = calculate_total_logistics_costs(data, documents_per_year)
     cost_per_minute = calculate_cost_per_minute(data)
-    total_operations_costs = calculate_total_operations_costs(
-        data, documents_per_year, cost_per_minute)
-    total_license_costs = 700  # Фиксированная стоимость лицензии
+    total_operations_costs = calculate_total_operations_costs(data, documents_per_year, cost_per_minute)
+    total_license_costs = calculate_total_license_costs(data, license_costs)
 
     return {
         "total_paper_costs": total_paper_costs,
